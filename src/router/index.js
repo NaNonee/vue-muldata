ea01f14a879a5dd98cf209c00fbe785c5769d7f0
wanght <kihtoner@163.com>
2020-08-17 13:09:27 +0800

    Element UI button测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import CopClass from '@/components/EnterPage'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/center/',
+      name: '/CopName',
+      component: CopClass
     }
   ]
 })
